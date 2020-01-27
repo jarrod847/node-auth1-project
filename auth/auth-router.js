3c35558dcb1d4268883534413036b6bbfe7f1f87
jarrodskahill <jarrodskahill@gmail.com>
2020-01-27 15:25:14 -0800

    MVP for day 1

---
+++ auth/auth-router.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const bc = require("bcryptjs")
+const Users = require('../users/users-model.js');
+
+router.post('/register', (req, res) => {
+  let user = req.body;
+  const hash = bc.hashSync(req.body.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bc.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = router;
